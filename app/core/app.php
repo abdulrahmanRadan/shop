ed9caf2864aa4075077c90bd31304b074aef5a93
abody-ali <aboda123li123@gmail.com>
2023-09-10 23:54:10 +0300

    end video3

--- app/core/app.php
+++ app/core/app.php
@@ -1,36 +1,38 @@
 <?php
 
-Class App 
+class App
 {
+    // protected Because i don't want them to be accessed to be able to access
     protected $controller = "home";
     protected $method = "index";
 
-    public function __construct(){
-        $url = $this-> parseURL();
-        show($url); 
+    public function __construct()
+    {
+        $url = $this->parseURL();
+        show($url);
 
-        if(file_exists("../app/controllers/" . strtolower($url[0]) . ".php")){
+        if (file_exists("../app/controllers/" . strtolower($url[0]) . ".php")) {
             $this->controller = strtolower($url[0]);
             unset($url[0]);
         }
 
         require "../app/controllers/" . $this->controller . ".php";
         $this->controller  = new $this->controller;
-        if(isset($url[1])){
+        if (isset($url[1])) {
             $url[1] = strtolower($url[1]);
-            if(method_exists($this->controller , $url[1])){
+            if (method_exists($this->controller, $url[1])) {
                 $this->method = $url[1];
                 unset($url[1]);
             }
         }
         show($url);
+    } // end __construct
 
-    }
-
-    private function parseURL() {
-        $url = isset($_GET['url']) ? $_GET['url'] : "home" ;
-        return explode("/", filter_var(trim($url, "/"), FILTER_SANITIZE_URL)); 
+    private function parseURL()
+    {
+        $url = isset($_GET['url']) ? $_GET['url'] : "home";
+        return explode("/", filter_var(trim($url, "/"), FILTER_SANITIZE_URL));
         // trim() ==> remove characters from both side of a string 
         // Filters a variable with a specified filter
-    }
-}
+    } //End parseURL
+}// end Class App
--- app/core/functions.php
+++ app/core/functions.php
@@ -1,9 +1,8 @@
-<?php 
+<?php
 
-    function show($data){
-        echo "<pre>";
-        print_r($data);
-        echo "<pre>";
-    }
-
-?>
+function show($data)
+{
+    echo "<pre>";
+    print_r($data);
+    echo "</pre>";
+}// End show Function
